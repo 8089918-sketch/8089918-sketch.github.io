7139dd36b9bf90da3799f309c9667561ac9a4cb6
8089918-sketch <8089918@opsb.info>
2026-09-18 18:05:56 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,49 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     
 
 
-    // TODO 2 - Create Platforms
-
-
-
+    //TODO 2 - Create Platforms
+createPlatform(350, 150, 150, 1000,"blue");
+createPlatform(1350, 420, 50, 50, "blue");
+createPlatform(150, 600, 200, 300,"blue");
+createPlatform(5, 650, 100, 40, "red");
+createPlatform(250, 500, 50, 20,"blue");
+createPlatform(250, 375, 50, 20, "blue");
+createPlatform( 150, 275, 50, 20,"blue");
+createPlatform(650, 150, 20, 20,"blue");
+createPlatform(750, 150, 500, 50, "blue");
+createPlatform(550, 600, 20, 20,"blue");
+createPlatform(550, 400, 20, 20,);
+createPlatform(1250, 0, 20, 425, );
+createPlatform(575, 400, 680, 20);
+createPlatform(700, 550, 1.5, 10,"red");
+createPlatform(800, 600, 1.5, 10, "red");
+createPlatform(950, 550, 400, 200, "red");
+createPlatform(1275, 300, 1.5, 10,"red",);
+createPlatform(1350, 250, 1.5, 10, "red",);
+createPlatform(1280, 150, 1.5, 10, "red");
+createPlatform(1350, 100, 150,10, "red");
 
     // TODO 3 - Create Collectables
-
-
+createCollectable("database", 260, 425, 0, 0.7);
+createCollectable("database", 450, 100, 0, 0.7);
+createCollectable("database", 1200, 100, 0, 0.7);
+createCollectable("database",1350,50,0,0.7)
+createCollectable("database",540, 525, 0, 0.7);
 
     
     // TODO 4 - Create Cannons
-
+createCannon("bottom", 850, 650)
+createCannon("bottom", 1000, 900)
+createCannon("bottom", 1200, 800)
+createCannon("right", 800, 0.5)
+createCannon("left", 300, 1500)
+createCannon("bottom", 1300, 1300)
 
     
-    
+  
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
